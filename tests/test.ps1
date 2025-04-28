547bb3bdb17a17f770b721021903ea9853672ef9
Stefan Fuertinger <stefan.fuertinger@esi-frankfurt.de>
2025-04-28 19:19:46 +0200

    NEW: Wrapped up first round of PowerShell tests
    - ensure vanilla installation + removal works
    - ensure existing ssh config is not perturbed
    
    On branch main
    Changes to be committed:
    	modified:   tests/test.ps1

--- tests/test.ps1
+++ tests/test.ps1
@@ -12,9 +12,10 @@ $Testsdir = Split-Path -Parent $MyInvocation.MyCommand.Path
 $VSRsetup = Join-Path $Testsdir "..\client\setup.ps1"
 $VSRsetup = Resolve-Path $VSRsetup
 
-# Test parameters (Set as appropriate)
+# Test parameters (set as appropriate)
 $VSRtester = "testuser"
 $VSRhead = "hpc-head.domain.local"
+$dummykey = "$sshdir\dummy"
 
 # Default ssh config/key location
 $sshdir = "$HOME\.ssh"
@@ -31,10 +32,18 @@ Host vscode-remote-hpc
     StrictHostKeyChecking no
 "@.Trim()
 
-# Run the main script w/input args to suppress interactive prompts
+# Run the setup w/input args to suppress interactive prompts
+Write-Host "Installing vscode-remote-hpc"
 & $VSRsetup $VSRtester $VSRhead
 
-# Read the actual file and regex to extract ssh configuration added by vscode-remote-hpc
+# First and foremost: ensure ssh config has been created
+if (Test-Path -Path $sshconfig) {
+    Write-Host "Test passed: ssh config file has been created" -ForegroundColor Green
+} else {
+    Write-Host "Test failed: ssh config file not found" -ForegroundColor Red
+}
+
+# Read the actual ssh config file and regex to extract ssh configuration added by vscode-remote-hpc
 $configblock = Get-Content $sshconfig -Raw
 $match = [regex]::Match($configblock, "(^Host\s+vscode-remote-hpc.*?)(?:(?:^Host|\z))", [System.Text.RegularExpressions.RegexOptions]::Multiline -bor [System.Text.RegularExpressions.RegexOptions]::Singleline)
 if ($match.Success) {
@@ -63,24 +72,15 @@ if ((Test-Path -Path $sshkey) and (Test-Path -Path "$sshkey.pub")) {
 }
 
 # Uninstall vscode-remote-hpc
+Write-Host "Uninstalling vscode-remote-hpc"
 & $VSRsetup $VSRtester $VSRhead
 
-# Ensure config has been wiped
-$match = [regex]::Match($configblock, "(^Host\s+vscode-remote-hpc.*?)(?:(?:^Host|\z))", [System.Text.RegularExpressions.RegexOptions]::Multiline -bor [System.Text.RegularExpressions.RegexOptions]::Singleline)
-if (-not $match.Success) {
-    Write-Host "Test failed: Host block not found in config file." -ForegroundColor Red
+# Ensure ssh config file is still present
+if (Test-Path -Path $sshconfig) {
+    Write-Host "Test passed: ssh config file still present" -ForegroundColor Green
 } else {
-    Write-Host "Test failed: Host block not found in config file." -ForegroundColor Red
-    exit 1
+    Write-Host "Test failed: ssh config file has been removed" -ForegroundColor Red
 }
-
-# Manually remove ssh key-pair
-Remove-Item -Path "$sshkey" -Force -ErrorAction SilentlyContinue
-Remove-Item -Path "$sshkey.pub" -Force -ErrorAction SilentlyContinue
-
-# Re-run installation and ensure backup copy of existing ssh configuration is created
-& $VSRsetup $VSRtester $VSRhead
-
 
 # Ensure backup copy of ssh configuration has been created
 if (Test-Path -Path $sshconfigbak) {
@@ -90,6 +90,117 @@ if (Test-Path -Path $sshconfigbak) {
     exit 1
 }
 
+# Ensure vscode-remote-hpc config block has been wiped
+$configblock = Get-Content $sshconfig -Raw
+$match = [regex]::Match($configblock, "(^Host\s+vscode-remote-hpc.*?)(?:(?:^Host|\z))", [System.Text.RegularExpressions.RegexOptions]::Multiline -bor [System.Text.RegularExpressions.RegexOptions]::Singleline)
+if (-not $match.Success) {
+    Write-Host "Test passed: Host block removed from config file." -ForegroundColor Green
+} else {
+    Write-Host "Test failed: Host block still present in config file." -ForegroundColor Red
+    exit 1
+}
 
-# Uninstall vscode-remote-hpc
+# Ensure ssh keys have been removed
+if ((Test-Path -Path $sshkey) or (Test-Path -Path "$sshkey.pub")) {
+    Write-Host "Test failed: ssh key pair still present." -ForegroundColor Red
+    exit 1
+} else {
+    Write-Host "Test passed: ssh keys have been removed." -ForegroundColor Green
+}
+
+# Manually create dummy ssh keys and config file entries
+ssh-keygen -q -f $dummykey -t ed25519 -N '""'
+$dummyblock = @"
+Host VeryImportant
+    User somebody
+    IdentityFile $dummykey
+"@
+Add-Content -Path $sshconfig -Value "`n$dummyblock"
+
+# Re-run the setup and ensure existing config + keys stay intact
+Write-Host "Re-install vscode-remote-hpc with existing ssh config + keys"
 & $VSRsetup $VSRtester $VSRhead
+
+# Ensure backup copy of ssh configuration has been created
+if (Test-Path -Path $sshconfigbak) {
+    Write-Host "Test passed: backup copy of ssh config generated." -ForegroundColor Green
+} else {
+    Write-Host "Test failed: backup copy of ssh config missing." -ForegroundColor Red
+    exit 1
+}
+
+# Ensure backup copy contains orig config
+$backupconfigblock = Get-Content $sshconfigbak -Raw
+$match = [regex]::Match($backupconfigblock, "(^Host\s+VeryImportant.*?)(?:(?:^Host|\z))", [System.Text.RegularExpressions.RegexOptions]::Multiline -bor [System.Text.RegularExpressions.RegexOptions]::Singleline)
+if ($match.Success) {
+    $actualconfig = $match.Groups[1].Value.Trim()
+    if ($actualconfig -eq $dummyblock) {
+        Write-Host "Test passed: Original config preserved in backup config file." -ForegroundColor Green
+    } else {
+        Write-Host "Test failed: Original config not matched in backup config file." -ForegroundColor Red
+        Write-Host "Expected:"
+        Write-Host $dummyblock
+        Write-Host "Actual:"
+        Write-Host $actualconfig
+        exit 1
+    }
+} else {
+    Write-Host "Test failed: Host block not found in backup config file." -ForegroundColor Red
+    exit 1
+}
+
+# Read the actual ssh config file and regex to extract ssh configuration added by vscode-remote-hpc
+# Additionally, ensure existing config is still in place
+$configblock = Get-Content $sshconfig -Raw
+$match = [regex]::Match($configblock, "(^Host\s+vscode-remote-hpc.*?)(?:(?:^Host|\z))", [System.Text.RegularExpressions.RegexOptions]::Multiline -bor [System.Text.RegularExpressions.RegexOptions]::Singleline)
+if ($match.Success) {
+    $actualconfig = $match.Groups[1].Value.Trim()
+    if ($actualconfig -eq $expectedconfig) {
+        Write-Host "Test passed: Host block written correctly." -ForegroundColor Green
+    } else {
+        Write-Host "Test failed: Host block content does not match expected." -ForegroundColor Red
+        Write-Host "Expected:"
+        Write-Host $expectedconfig
+        Write-Host "Actual:"
+        Write-Host $actualconfig
+        exit 1
+    }
+} else {
+    Write-Host "Test failed: Host block not found in config file." -ForegroundColor Red
+    exit 1
+}
+$match = [regex]::Match($configblock, "(^Host\s+VeryImportant.*?)(?:(?:^Host|\z))", [System.Text.RegularExpressions.RegexOptions]::Multiline -bor [System.Text.RegularExpressions.RegexOptions]::Singleline)
+if ($match.Success) {
+    $actualconfig = $match.Groups[1].Value.Trim()
+    if ($actualconfig -eq $dummyblock) {
+        Write-Host "Test passed: Existing ssh config has been preserved." -ForegroundColor Green
+    } else {
+        Write-Host "Test failed: Existing ssh config not found in $sshconfig." -ForegroundColor Red
+        Write-Host "Expected:"
+        Write-Host $dummyblock
+        Write-Host "Actual:"
+        Write-Host $actualconfig
+        exit 1
+    }
+} else {
+    Write-Host "Test failed: Host block not found in config file." -ForegroundColor Red
+    exit 1
+}
+
+# Ensure vscode-remote-hpc ssh keys have been generated
+if ((Test-Path -Path $sshkey) and (Test-Path -Path "$sshkey.pub")) {
+    Write-Host "Test passed: ssh keys generated." -ForegroundColor Green
+} else {
+    Write-Host "Test failed: ssh keys not found." -ForegroundColor Red
+    exit 1
+}
+
+# Ensure existing ssh keys have been preserved
+if ((Test-Path -Path $dummykey) and (Test-Path -Path "$dummykey.pub")) {
+    Write-Host "Test passed: Existing ssh keys have been preserved." -ForegroundColor Green
+} else {
+    Write-Host "Test failed: Existing ssh keys not found." -ForegroundColor Red
+    exit 1
+}
+
+Write-Host "ALL PASSED"
