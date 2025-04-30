a4af049d77ed59827fff7831be6b9911a0830660
Stefan Fuertinger <stefan.fuertinger@esi-frankfurt.de>
2025-04-30 12:19:08 +0200

    CHG: Modifed macOS installer to work w/CLI args
    - so that testing actually does something
    
    On branch main
    Changes to be committed:
    	modified:   client/setup.sh

--- client/setup.sh
+++ client/setup.sh
@@ -8,6 +8,15 @@
 # SPDX-License-Identifier: MIT
 #
 
+# Either query for HPC username and headnode or take CLI arguments
+# (mainly intended for testing!)
+if [ -n "$1" ]; then
+    uname="$1"
+fi
+if [ -n "$2" ]; then
+    headnode="$2"
+fi
+
 # Default ssh config/key location
 sshdir="${HOME}/.ssh"
 sshconfig="${sshdir}/config"
@@ -18,10 +27,14 @@ echo "--- This script sets up VS Code remote connections to the HPC cluster ---"
 
 # Check if vscode-remote-hpc has already been setup
 if [[ -f "${sshconfig}" && -f "${sshkey}" ]]; then
-    echo "It seems vscode-remote-hpc is already installed. How do you want to proceed?"
-    echo "1. Abort"
-    echo "2. Uninstall"
-    read -p "Please choose an option (1 or 2): " choice </dev/tty
+    if [[ $# -eq 0 ]]; then
+        echo "It seems vscode-remote-hpc is already installed. How do you want to proceed?"
+        echo "1. Abort"
+        echo "2. Uninstall"
+        read -p "Please choose an option (1 or 2): " choice </dev/tty
+    else
+        choice='2'
+    fi
     case "${choice}" in
         1)
             exit
@@ -57,9 +70,13 @@ if [[ -f "${sshconfig}" && -f "${sshkey}" ]]; then
 fi
 
 # Query account/head node information
-read -p "Please enter your HPC username: " uname </dev/tty
-echo "Please enter the IP address or hostname of the cluster head node"
-read -p " (hub.esi.local at ESI, or 192.168.161.221 at CoBIC): " headnode </dev/tty
+if [[ -z "${uname+x}" ]]; then
+    read -p "Please enter your HPC username: " uname </dev/tty
+fi
+if [[ -z "${headnode+x}" ]]; then
+    echo "Please enter the IP address or hostname of the cluster head node"
+    read -p " (hub.esi.local at ESI, or 192.168.161.221 at CoBIC): " headnode </dev/tty
+fi
 
 # Put together configuration block for ssh config
 configblock="Host vscode-remote-hpc
@@ -94,9 +111,13 @@ fi
 
 # If it does not exist already, create a new ssh key for vscode-remote-hpc
 if [ ! -f "${sshkey}" ]; then
-    read -p "About to create and upload an ssh key to ${headnode}. You will be prompted for your cluster password. Press any key to continue " ans </dev/tty
+    if [[ $# -eq 0 ]]; then
+        read -p "About to create and upload an ssh key to ${headnode}. You will be prompted for your cluster password. Press any key to continue " ans </dev/tty
+    fi
     ssh-keygen -q -f "${sshkey}" -t ed25519 -N ""
-    ssh-copy-id -i "${sshkey}" "${headnode}"
+    if [[ $# -eq 0 ]]; then
+        ssh-copy-id -i "${sshkey}" "${headnode}"
+    fi
 else
     echo "VS Code remote ssh key already exists. No changes made."
 fi
