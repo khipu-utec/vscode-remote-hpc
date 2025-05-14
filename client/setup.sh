29d9a99427063fc6458aae220bc195ff3ef51ae1
Stefan Fuertinger <stefan.fuertinger@esi-frankfurt.de>
2025-05-14 11:38:47 +0200

    CHG: Updated macOS/Linux installer
    - pretty-print messages to clearly distinct b/w stuff we're emitting
      and output from 3rd party tools (cf. #2)
    - included a trap that cleans up in case things go wrong
    
    On branch dev
    Changes to be committed:
    	modified:   client/setup.sh

--- client/setup.sh
+++ client/setup.sh
@@ -10,6 +10,26 @@
 
 set -e
 
+# Keep it simple in case we're running in a POSIX-shell
+posix_abort() {
+  printf "ERROR: %s\n" "$@" >&2
+  exit 1
+}
+
+# Fail fast with a concise message when not using bash
+# Single brackets are needed here for POSIX compatibility
+# shellcheck disable=SC2292
+if [ -z "${BASH_VERSION:-}" ]
+then
+  posix_abort "Bash is required to interpret this script."
+fi
+
+# Default ssh config/key location
+sshdir="${HOME}/.ssh"
+sshconfig="${sshdir}/config"
+sshconfigbak="${sshconfig}_$(date +%Y-%m-%d).vsr"
+sshkey="${sshdir}/vscode-remote-hpc"
+
 # Either query for HPC username and headnode or take CLI arguments
 # (mainly intended for testing!)
 if [ -n "$1" ]; then
@@ -19,20 +39,95 @@ if [ -n "$2" ]; then
     headnode="$2"
 fi
 
-# Default ssh config/key location
-sshdir="${HOME}/.ssh"
-sshconfig="${sshdir}/config"
-sshconfigbak="${sshconfig}_$(date +%Y-%m-%d).vsr"
-sshkey="${sshdir}/vscode-remote-hpc"
+# String formatters to prettify output
+if [[ -t 1 ]]; then
+  tty_escape() { printf "\033[%sm" "$1"; }
+else
+  tty_escape() { :; }
+fi
+tty_mkbold() { tty_escape "1;$1"; }
+tty_blue="$(tty_mkbold 34)"
+tty_green="$(tty_mkbold 32)"
+tty_red="$(tty_mkbold 31)"
+tty_bold="$(tty_mkbold 39)"
+tty_reset="$(tty_escape 0)"
 
-echo "--- This script sets up VS Code remote connections to the HPC cluster ---"
+shell_join()
+{
+  local arg
+  printf "%s" "$1"
+  shift
+  for arg in "$@"
+  do
+    printf " "
+    printf "%s" "${arg// /\ }"
+  done
+}
+
+chomp()
+{
+  printf "%s" "${1/"$'\n'"/}"
+}
+
+announce()
+{
+  printf "${tty_green}>>> %s <<<${tty_reset}\n" "$(shell_join "$@")"
+}
+
+info()
+{
+  printf "${tty_blue}%s${tty_reset}\n" "$(shell_join "$@")"
+}
+
+error()
+{
+  printf "${tty_red}FAILED:${tty_bold} %s${tty_reset}\n" "$(chomp "$1")"
+}
+
+# Ensure errors are communicated properly
+on_exit() {
+    if [ $? -ne 0 ]; then
+        error "Setup encountered an error. Examine previous error messages for details"
+        cleanup
+        exit 1
+    fi
+    info "Bye"
+}
+trap 'on_exit 2> /dev/null' SIGHUP SIGTERM SIGKILL EXIT
+
+cleanup(){
+    if [[ -f "${sshconfig}" ]]; then
+        cp -f "${sshconfig}" "${sshconfigbak}"
+        info "Wrote backup-copy ${sshconfigbak} of current ssh configuration file"
+        awk '
+           BEGIN {skip=0}
+           /^[ \t]*Host[ \t]+vscode-remote-hpc[ \t]*$/ {skip=1; next}
+           skip && /^[ \t]/ {next}
+           skip && /^[[:space:]]*$/ {next}
+           skip {skip=0}
+           {print}
+        ' "${sshconfig}" > "${sshconfig}.tmp" && mv "${sshconfig}.tmp" "${sshconfig}"
+        info "Block for vscode-remote-hpc has been removed from ${sshconfig} (if it was present)."
+    else
+        info "${sshconfig} does not exist. Nothing to remove."
+    fi
+    info "Removing generated ssh key-pair"
+    rm -f "${sshkey}"
+    rm -f "${sshkey}.pub"
+    info "Done"
+}
+
+# ----------------------------------------------------------------------
+#                    START OF INSTALLATION SCRIPT
+# ----------------------------------------------------------------------
+announce "This script sets up VS Code remote connections to the HPC cluster"
 
 # Check if vscode-remote-hpc has already been setup
 if [[ -f "${sshconfig}" && -f "${sshkey}" ]]; then
     if [[ $# -eq 0 ]]; then
-        echo "It seems vscode-remote-hpc is already installed. How do you want to proceed?"
-        echo "1. Abort"
-        echo "2. Uninstall"
+        info "It seems vscode-remote-hpc is already installed. How do you want to proceed?"
+        info "1. Abort"
+        info "2. Uninstall"
         read -p "Please choose an option (1 or 2): " choice </dev/tty
     else
         choice='2'
@@ -42,30 +137,12 @@ if [[ -f "${sshconfig}" && -f "${sshkey}" ]]; then
             exit
             ;;
         2)
-            if [[ -f "${sshconfig}" ]]; then
-                cp -f "${sshconfig}" "${sshconfigbak}"
-                echo "Wrote backup-copy ${sshconfigbak} of current ssh configuration file"
-                awk '
-                   BEGIN {skip=0}
-                   /^[ \t]*Host[ \t]+vscode-remote-hpc[ \t]*$/ {skip=1; next}
-                   skip && /^[ \t]/ {next}
-                   skip && /^[[:space:]]*$/ {next}
-                   skip {skip=0}
-                   {print}
-                ' "${sshconfig}" > "${sshconfig}.tmp" && mv "${sshconfig}.tmp" "${sshconfig}"
-                echo "Block for vscode-remote-hpc has been removed from ${sshconfig} (if it was present)."
-            else
-                echo "${sshconfig} does not exist. Nothing to remove."
-            fi
-            echo "Removing generated ssh key-pair"
-            rm -f "${sshkey}"
-            rm -f "${sshkey}.pub"
-            echo "Done"
-            echo "All cleaned up, vscode-remote-hpc has been uninstalled. Bye. "
+            cleanup
+            announce "All cleaned up, vscode-remote-hpc has been uninstalled."
             exit
             ;;
         *)
-            echo "Invalid choice. Aborting."
+            error "Invalid choice. Aborting."
             exit
             ;;
     esac
@@ -76,8 +153,8 @@ if [[ -z "${uname+x}" ]]; then
     read -p "Please enter your HPC username: " uname </dev/tty
 fi
 if [[ -z "${headnode+x}" ]]; then
-    echo "Please enter the IP address or hostname of the cluster head node"
-    read -p " (hub.esi.local at ESI, or 192.168.161.221 at CoBIC): " headnode </dev/tty
+    info "Please enter the IP address or hostname of the cluster head node"
+    read -p "(hub.esi.local at ESI, or 192.168.161.221 at CoBIC): " headnode </dev/tty
 fi
 
 # Put together configuration block for ssh config
@@ -106,15 +183,17 @@ fi
 if ! grep -qE '^[Hh]ost[[:space:]]+vscode-remote-hpc\b' "${sshconfig}"; then
     echo "" >> "${sshconfig}"
     echo "${configblock}" >> "${sshconfig}"
-    echo "Updated ssh configuration"
+    info "Updated ssh configuration"
 else
-    echo "VS Code remote HPC configuration already exists. No changes made."
+    info "VS Code remote HPC configuration already exists. No changes made."
 fi
 
 # If it does not exist already, create a new ssh key for vscode-remote-hpc
 if [ ! -f "${sshkey}" ]; then
     if [[ $# -eq 0 ]]; then
-        read -p "About to create and upload an ssh key to ${headnode}. You will be prompted for your cluster password. Press any key to continue " ans </dev/tty
+        info "About to create and upload an ssh key to ${headnode}"
+        info "You will be prompted for your cluster password."
+        read -p "Press any key to continue " ans </dev/tty
     fi
     machine="${HOST}"
     if [ -z "${machine}" ]; then
@@ -125,7 +204,7 @@ if [ ! -f "${sshkey}" ]; then
         ssh-copy-id -i "${sshkey}" "${uname}@${headnode}"
     fi
 else
-    echo "VS Code remote ssh key already exists. No changes made."
+    info "VS Code remote ssh key already exists. No changes made."
 fi
 
-echo "-- All Done ---"
+announce "All Done"
